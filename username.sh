add8bbb5708246b35f79616ce03fbc3fc610b781
Evelyn Lawrie <evelynmarielawrie@gmail.com>
2020-11-13 14:05:02 -0500

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 # username.sh
 # Evelyn Lawrie
-echo "Enter a Username: "
+echo "Your username must contain between three and twelve lowercase letters, digits, and underscores. It must start with a lowercase letter."
+echo "Enter a username: "
 read -r NAME
-while echo "$NAME" | grep -E -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid username - between three and twelve lowercase letters, digits and underscores only! Remember to start with a lowercase letter!"
+	echo "Enter a username: "
 	read -r NAME
 done
-echo "Thank you"
+echo "Thank you!"
